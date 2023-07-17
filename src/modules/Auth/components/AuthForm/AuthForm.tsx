7fecc32bb5019c0cb898b950c4f7ab7c4a8e33b9
Ilya misyura <ilya.mi@digitrade.co.il>
2023-07-17 22:49:49 +0300

    add restore password and forgot password

--- src/modules/Auth/components/AuthForm/AuthForm.tsx
+++ src/modules/Auth/components/AuthForm/AuthForm.tsx
@@ -14,10 +14,13 @@ type Inputs = {
     userExId?: string,
     phone?: string
     //------------------
+    token?: string
+
+
 
   };
 
-  type AuthType = "login" | "register" | "validation" | "twoFactor";
+  type AuthType = "login" | "register" | "validation" | "twoFactor" | "forgotPassword" | "validMailPasswordResore" | "restorePassword";
 
 const AuthForm = () => {
 
@@ -25,30 +28,52 @@ const AuthForm = () => {
     const [type, setType] = useState<AuthType>('login')
     const { register, handleSubmit, reset , formState: { errors } } = useForm<Inputs>();
   
-    const handleSwitch = (type: 'login' | 'register' | 'validation' | "twoFactor") => {
+    const handleSwitch = (type: 'login' | 'register' | 'validation' | "twoFactor" | "forgotPassword" | "validMailPasswordResore" | "restorePassword" ) => {
       reset()
       setType(type)
 
     }
     const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        if(otp) {
+            data.token = otp
+        }
         const response = await AuthService.auth(type, data)
         if(type === 'validation') {
+            reset()
             if(response) {
                 setType('twoFactor')
             }
         }
         if(type === 'twoFactor') {
+            reset()
+            setOtp('')
             if(response) {
                 setType('register')
             } 
         }
+
+        if(type === 'forgotPassword') {
+           setType('validMailPasswordResore') 
+        }   
+
+        if(type === 'validMailPasswordResore') {
+            setType('restorePassword') 
+         }   
+
+
+         if(type === 'restorePassword') {
+            setType('login')
+         }
+
     }
+
+
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className='border border-gray-500 rounded-lg text-center px-12 py-10'>
             <h1 id="auth-heading" data-testid="auth-heading" className='font-bold text-xl'>{type}</h1>
 
-            {(type === 'login' || type === 'register') &&
+            {(type === 'login' || type === 'register' || type === 'forgotPassword') &&
                 <div>
                     <input id="email" {...register("email")}  placeholder='email' className='px-2 py-2 border border-gray-400 rounded-md w-96 m-2'/>
                     {errors.email && <span>This field is required</span>}
@@ -81,7 +106,7 @@ const AuthForm = () => {
                 </>
             }
             {
-                (type === 'login' || type === 'register') &&
+                (type === 'login' || type === 'register' || type === 'restorePassword') &&
                 <div>
                     <input id="password" {...register("password", { required: true })} placeholder='password' className='px-2 py-2 border border-gray-400 rounded-md w-96 m-2'/>
                     {errors.password && <span>This field is required</span>}
@@ -89,7 +114,7 @@ const AuthForm = () => {
 
             }
             {
-               type === 'twoFactor' &&
+               (type === 'twoFactor' || type === 'validMailPasswordResore') &&
                <div className='flex justify-center items-center'>
                     <OtpInput
                     value={otp}
@@ -101,11 +126,27 @@ const AuthForm = () => {
                 </div>
             }
 
-            <div className='auth flex items-center justify-center gap-4 text-lg'>
-                <span id="login-btn" data-testid="login-btn" className='cursor-pointer' onClick={() => handleSwitch('login')}>Login</span>
-                <span>|</span>
-                <span id="register-btn" data-testid="register-btn" className='cursor-pointer' onClick={() => handleSwitch('validation')}>Register</span>
-            </div>
+
+            { (type === 'login' || type === 'register' || type === 'validation' ) &&
+                <>
+                <div className='auth flex items-center justify-center gap-4 text-lg'>
+                    <span id="login-btn" data-testid="login-btn" className='cursor-pointer' onClick={() => handleSwitch('login')}>Login</span>
+                    <span>|</span>
+                    <span id="register-btn" data-testid="register-btn" className='cursor-pointer' onClick={() => handleSwitch('validation')}>Register</span>
+                </div>
+                <div>
+                    <span className='underline cursor-pointer' onClick={() => handleSwitch('forgotPassword')}>forgot password?</span>
+                </div>
+                </>
+            } 
+
+            {/* {
+                type === 'validMailPasswordResore' &&
+                <div className='py-2'>
+                    <button className='underline cursor-pointer' onClick={() => handleSwitch('forgotPassword')}>dont get mail/sms ?</button>
+                </div>
+            } */}
+
             <div className='m-2'>
                 <button id="auth-btn" data-testid="auth-btn" type="submit" className='font-bold bg-blue-400 text-white py-2 rounded-xl px-12 hover:bg-blue-600'>{type}</button>
             </div>
--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -3,8 +3,17 @@ import { IUser } from "../../types/user.interface";
 import { saveToStorage } from "./auth.helper";
 
 type Auth = {
-    email: string;
-    password: string;
+    email: string,
+    password: string,
+    //--for register----
+    firstName?: string,
+    lastName?: string,
+    //------------------
+    //--for validation--
+    userExId?: string,
+    phone?: string
+    //------------------
+    token?: string
 }
 
 type AuthResponse = {
@@ -14,11 +23,18 @@ type AuthResponse = {
 }
 
 export const AuthService = {
-    async auth(type: 'login' | 'register' | 'validation' | 'twoFactor', data: Auth){
+    async auth(type: 'login' | 'register' | 'validation' | 'twoFactor' | 'forgotPassword' | 'restorePassword' | 'validMailPasswordResore', data: Auth){
+        console.log('data',data)
         if(type === 'validation'){
             return true;
         } else if(type === 'twoFactor') {
             return true;
+        } else if (type === 'forgotPassword') {
+            return true;
+        } else if (type === 'validMailPasswordResore') {
+            return true   
+        } else if (type === 'restorePassword') {
+            return true
         } else {
             const response = await axios<AuthResponse>({
                 url: `http://localhost:4000/auth/${type}`,
