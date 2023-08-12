0f88d7b6358300b691915c18dea31ab981d1ff52
Ilya misyura <ilya.mi@digitrade.co.il>
2023-08-12 14:18:31 +0300

    add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import RouterApp from './RouterApp';
-import MobileNavigation from './components/ui/MobileNavigation/MobileNavigation';
 
 function App() {
   return (
     <>
       <RouterApp/>
-      <MobileNavigation/>
     </>
   );
 }
--- src/components/ui/MobileNavigation/MobileNavigation.tsx
+++ src/components/ui/MobileNavigation/MobileNavigation.tsx
@@ -1,30 +1,31 @@
 import React, { useState } from 'react';
-import {AiOutlineHome, AiOutlineMessage, AiOutlineShoppingCart} from 'react-icons/ai'
-import {BsPerson} from 'react-icons/bs'
-import {FiSettings} from 'react-icons/fi'
-import './MobileNavigation.styles.scss'
+import { AiOutlineHome, AiOutlineMessage, AiOutlineShoppingCart } from 'react-icons/ai';
+import { BsPerson } from 'react-icons/bs';
+import { FiSettings } from 'react-icons/fi';
+import './MobileNavigation.styles.scss';
+import { useNavigate } from 'react-router-dom';
+
 const MobileNavigation = () => {
-    const [active, setActive] = useState('')
+    const [active, setActive] = useState('');
+    const navigate = useNavigate();
     const data = [
-        {id:1, title:'בית', icon:<AiOutlineHome/>},
-        {id:2, title:'הודעות', icon: <AiOutlineMessage/>},
-        {id:3, title:'פרופיל', icon: <BsPerson/>},
-        {id:4, title:'עגלה', icon:<AiOutlineShoppingCart/>},
-        {id:5, title:'הגדרות', icon:<FiSettings/>},
-    ]
+        { id: 1, title: 'בית', icon: <AiOutlineHome /> },
+        { id: 2, title: 'הודעות', icon: <AiOutlineMessage /> },
+        { id: 3, title: 'פרופיל', icon: <BsPerson /> },
+        { id: 4, title: 'עגלה', icon: <AiOutlineShoppingCart /> },
+        { id: 5, title: 'הגדרות', icon: <FiSettings /> },
+    ];
     return (
         <div className='navigation'>
             <ul>
-                {data.map((item) => 
-                    <li className={item.title === active ? 'active' :''} key={item.id} onClick={() => setActive(item.title)}>
-                        <a href='#'>
-                            <span className='icon'>
-                                {item.icon}
-                            </span>
+                {data.map((item) => (
+                    <li className={item.title === active ? 'active' : ''} key={item.id} onClick={() => setActive(item.title)}>
+                        <a href="javascript:void(0)" onClick={() => navigate('/')}>
+                            <span className='icon'>{item.icon}</span>
                             <span className='text'>{item.title}</span>
                         </a>
                     </li>
-                )}
+                ))}
                 <div className='indicator'></div>
             </ul>
         </div>
